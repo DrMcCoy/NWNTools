f4c799bea027a9d58a22741e40f6feedcfe3c329
sumpfork <sumpfork>
2004-05-05 19:52:17 +0000

    moving into nwntools package version to 1.1 (it supports HotU, after all)

--- nwnnsscomp/python/setup.py
+++ nwnnsscomp/python/setup.py
@@ -4,7 +4,7 @@ import sys
 link_args = []
 if sys.platform == 'darwin':
     link_args.append('-Wl,-flat_namespace,-U,_environ')
-module1 = Extension('nsscompiler',
+module1 = Extension('nwntools.nsscompiler',
                     libraries = ['nsc','nwn'],
                     library_dirs = ['../../_NscLib','../../_NwnLib'],
                     extra_compile_args=['-Wno-sign-compare'],
@@ -12,7 +12,7 @@ module1 = Extension('nsscompiler',
                     extra_link_args=link_args)
 
 setup (name = 'nwntools',
-       version = '1.01',
+       version = '1.1',
        description = 'nss compiler',
        author = 'By Torlack, python interface by sumpfork',
        author_email = 'sumpfork@users.sf.net',
